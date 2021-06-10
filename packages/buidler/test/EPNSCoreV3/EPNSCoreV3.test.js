35a02dda9de8a1819c3f33fe7749bc985d4a952f
zaryab2000 <zaryabafser2000@gmail.com>
2021-06-10 12:50:57 +0530

    completed All Test Cases Scripts

---
+++ packages/buidler/test/EPNSCoreV3/EPNSCoreV3.test.js
@@ -0,0 +1,128 @@
+const { ethers } = require("hardhat");
+const { use, expect } = require("chai");
+const { solidity } = require("ethereum-waffle");
+const {
+  advanceBlockTo,
+  latestBlock,
+  advanceBlock,
+  increase,
+  increaseTo,
+  latest,
+} = require("../time");
+const { calcChannelFairShare, calcSubscriberFairShare, getPubKey, bn, tokens, tokensBN, bnToInt, ChannelAction, readjustFairShareOfChannels, SubscriberAction, readjustFairShareOfSubscribers } = require("../../helpers/utils");
+
+use(solidity);
+
+describe("EPNSCoreV2 tests", function () {
+  const AAVE_LENDING_POOL = "0x1c8756FD2B28e9426CDBDcC7E3c4d64fa9A54728";
+  const DAI = "0xf80A32A835F79D7787E8a8ee5721D0fEaFd78108";
+  const ADAI = "0xcB1Fe6F440c49E9290c3eb7f158534c2dC374201";
+  const referralCode = 0;
+  const ADD_CHANNEL_MIN_POOL_CONTRIBUTION = tokensBN(50)
+  const ADD_CHANNEL_MAX_POOL_CONTRIBUTION = tokensBN(250000 * 50)
+  const DELEGATED_CONTRACT_FEES = bn(2).mul(10).pow(17);
+  const ADJUST_FOR_FLOAT = bn(10 ** 7)
+  const delay = 0; // uint for the timelock delay
+
+  const forkAddress = {
+    address: "0xe2a6cf5f463df94147a0f0a302c879eb349cb2cd",
+  };
+
+  let EPNS;
+  let GOVERNOR;
+  let PROXYADMIN;
+  let LOGIC;
+  let LOGICV2;
+  let LOGICV3;
+  let EPNSProxy;
+  let EPNSCoreV2Proxy;
+  let TIMELOCK;
+  let ADMIN;
+  let MOCKDAI;
+  let ADAICONTRACT;
+  let ALICE;
+  let BOB;
+  let CHARLIE;
+  let CHANNEL_CREATOR;
+  let ADMINSIGNER;
+  let ALICESIGNER;
+  let BOBSIGNER;
+  let CHARLIESIGNER;
+  let CHANNEL_CREATORSIGNER;
+  const ADMIN_OVERRIDE = "";
+
+  const coder = new ethers.utils.AbiCoder();
+  // `beforeEach` will run before each test, re-deploying the contract every
+  // time. It receives a callback, which can be async.
+
+  before(async function (){
+    const MOCKDAITOKEN = await ethers.getContractFactory("MockDAI");
+    MOCKDAI = MOCKDAITOKEN.attach(DAI);
+
+    const ADAITOKENS = await ethers.getContractFactory("MockDAI");
+    ADAICONTRACT = ADAITOKENS.attach(ADAI);
+  });
+
+  beforeEach(async function () {
+    // Get the ContractFactory and Signers here.
+    const [
+      adminSigner,
+      aliceSigner,
+      bobSigner,
+      charlieSigner,
+      channelCreatorSigner,
+    ] = await ethers.getSigners();
+
+    ADMINSIGNER = adminSigner;
+    ALICESIGNER = aliceSigner;
+    BOBSIGNER = bobSigner;
+    CHARLIESIGNER = charlieSigner;
+    CHANNEL_CREATORSIGNER = channelCreatorSigner;
+
+    ADMIN = await adminSigner.getAddress();
+    ALICE = await aliceSigner.getAddress();
+    BOB = await bobSigner.getAddress();
+    CHARLIE = await charlieSigner.getAddress();
+    CHANNEL_CREATOR = await channelCreatorSigner.getAddress();
+
+    const EPNSTOKEN = await ethers.getContractFactory("EPNS");
+    EPNS = await EPNSTOKEN.deploy();
+
+    const EPNSCoreV2 = await ethers.getContractFactory("EPNSCoreV2");
+    LOGIC = await EPNSCoreV2.deploy();
+
+    const TimeLock = await ethers.getContractFactory("Timelock");
+    TIMELOCK = await TimeLock.deploy(ADMIN, delay);
+
+    const proxyAdmin = await ethers.getContractFactory("EPNSAdmin");
+    PROXYADMIN = await proxyAdmin.deploy();
+    await PROXYADMIN.transferOwnership(TIMELOCK.address);
+
+    const EPNSPROXYContract = await ethers.getContractFactory("EPNSProxy");
+    EPNSProxy = await EPNSPROXYContract.deploy(
+      LOGIC.address,
+      ADMINSIGNER.address,
+      AAVE_LENDING_POOL,
+      DAI,
+      ADAI,
+      referralCode
+    );
+
+    await EPNSProxy.changeAdmin(ALICESIGNER .address);
+    EPNSCoreV2Proxy = EPNSCoreV2.attach(EPNSProxy.address)
+  });
+
+  afterEach(function () {
+    EPNS = null
+    LOGIC = null
+    TIMELOCK = null
+    EPNSProxy = null
+    EPNSCoreV2Proxy = null
+  });
+
+//   it("should return updated DELEGATED_CONTRACT_FEES", async function(){
+//     const delegatedContractFees = await EPNSCoreV2Proxy.DELEGATED_CONTRACT_FEES();
+
+//     expect(delegatedContractFees).to.equal(DELEGATED_CONTRACT_FEES);
+//   });
+});
