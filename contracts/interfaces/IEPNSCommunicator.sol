4548bfc2ba1bf4a93a592e1d617e11bfb78364dd
zaryab2000 <zaryabafser2000@gmail.com>
2021-07-28 12:50:27 +0530

    Added Interface of Communicator Protocol functions

--- contracts/interfaces/IEPNSCommunicator.sol
+++ contracts/interfaces/IEPNSCommunicator.sol
@@ -1,8 +1,8 @@
 pragma solidity >=0.6.0 <0.7.0;
 
 interface IEPNSCommunicator {
-    function subscribe(
-      address _channel,
-      address _user
-    ) external returns(bool); 
+ 	function subscribeViaCore(address _channel, address _user) external returns(bool);
+ 	function addDelegate(address _delegate) external returns(bool);
+ 	function removeDelegate(address _delegate) external returns(bool);
+
 }
