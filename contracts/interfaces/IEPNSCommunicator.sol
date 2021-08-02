9706112149d1d50f3c6be45af3191a0f4d765a6e
zaryab2000 <zaryabafser2000@gmail.com>
2021-08-02 18:07:42 +0530

    Fixed interfaces for communicator

--- contracts/interfaces/IEPNSCommunicator.sol
+++ contracts/interfaces/IEPNSCommunicator.sol
@@ -2,7 +2,4 @@ pragma solidity >=0.6.0 <0.7.0;
 
 interface IEPNSCommunicator {
  	function subscribeViaCore(address _channel, address _user) external returns(bool);
- 	function addDelegate(address _delegate) external returns(bool);
- 	function removeDelegate(address _delegate) external returns(bool);
-
 }
