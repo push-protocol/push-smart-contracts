6d3fcf80ad4740857794c9d73e9edf3421acfd0c
zaryab2000 <zaryabafser2000@gmail.com>
2021-09-01 16:57:58 +0530

    Fixed and removed commented lines

--- test/v4/EPNSCoreV4.NotifAdvancedTest.js
+++ test/v4/EPNSCoreV4.NotifAdvancedTest.js
@@ -289,10 +289,3 @@ const { ethers } = require("hardhat");
       });
   });
 });
-
-// const channelArray = [
-//   0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266,
-//   0x70997970C51812dc3A010C7d01b50e0d17dc79C8,
-//   0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC];
-// const hash = ethers.utils.sha256([ "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266" ]);
-// console.log(hash)
