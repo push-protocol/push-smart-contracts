94bb3ee9b1fd7403f2d56ebdaae8f85069a3a2b2
zaryab2000 <zaryabafser2000@gmail.com>
2021-09-10 09:05:05 +0530

    Added imperative function signatures for PUSH Interface

--- contracts/interfaces/IPUSH.sol
+++ contracts/interfaces/IPUSH.sol
@@ -1,6 +1,8 @@
 pragma solidity >=0.6.0 <0.7.0;
 
 interface IPUSH {
-  function returnHolderUnits(address account, uint atBlock) external view returns (uint);
+  function born() external view returns(uint);
+  function totalSupply() external view returns(uint);
   function resetHolderWeight(address holder) external;
+  function returnHolderUnits(address account, uint atBlock) external view returns (uint);
 }
