13e2827d1560979d197230a5b480acbeaf9a59b1
zaryab2000 <zaryabafser2000@gmail.com>
2021-09-21 18:34:49 +0530

    Updated Core Proxy

--- contracts/EPNSCoreProxy.sol
+++ contracts/EPNSCoreProxy.sol
@@ -10,6 +10,7 @@ contract EPNSCoreProxy is TransparentUpgradeableProxy {
     constructor(
         address _logic,
         address _governance,
+        address _pushChannelAdmin,
         address _pushTokenAddress,
         address _wethAddress,
         address _uniswapRouterAddress,
@@ -17,6 +18,6 @@ contract EPNSCoreProxy is TransparentUpgradeableProxy {
         address _daiAddress,
         address _aDaiAddress,
         uint _referralCode
-    ) public payable TransparentUpgradeableProxy(_logic, _governance, abi.encodeWithSignature('initialize(address,address,address,address,address,address,address,uint256)', _governance, _pushTokenAddress, _wethAddress, _uniswapRouterAddress, _lendingPoolProviderAddress, _daiAddress, _aDaiAddress,_referralCode)) {}
+    ) public payable TransparentUpgradeableProxy(_logic, _governance, abi.encodeWithSignature('initialize(address,address,address,address,address,address,address,uint256)', _pushChannelAdmin, _pushTokenAddress, _wethAddress, _uniswapRouterAddress, _lendingPoolProviderAddress, _daiAddress, _aDaiAddress,_referralCode)) {}
 
 }
