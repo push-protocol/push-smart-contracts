c045aca8290feb99dd4b814a690a0c71057c3ca5
zaryab2000 <zaryabafser2000@gmail.com>
2021-11-09 17:22:39 +0530

    Removed unwanted lines

--- scripts/7_polygonDeployEPNSCommV1.js
+++ scripts/7_polygonDeployEPNSCommV1.js
@@ -27,15 +27,6 @@ async function main() {
   console.log(chalk.bgWhite.bold.black(`\n\t\t\t\n ✅ Version upgraded    \n\t\t\t\n`))
 }
 
-// IMOORTANT: ADD THIS FROM CONFIG
-// const DAI = "0xf80A32A835F79D7787E8a8ee5721D0fEaFd78108";
-// const ADAI = "0xcB1Fe6F440c49E9290c3eb7f158534c2dC374201";
-// const WETH = "0xc778417E063141139Fce010982780140Aa0cD5Ab";
-// const PUSH = "0xf418588522d5dd018b425E472991E52EBBeEEEEE";
-// const UNISWAP_ROUTER = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-// const AAVE_LENDING_POOL = "0x1c8756FD2B28e9426CDBDcC7E3c4d64fa9A54728";
-// const referralCode = "0";
-
 async function setupAllContracts(versionDetails) {
   let deployedContracts = []
   console.log("📡 Deploy \n");
@@ -47,39 +38,11 @@ async function setupAllContracts(versionDetails) {
 
   // const admin = '0xA1bFBd2062f298a46f3E4160C89BEDa0716a3F51'; //admin of timelock, gets handed over to the governor.
 
-  const delay = 0; // uint for the timelock delay
-
-  // const epns = await deploy("EPNS");
-  // const epns = await deployContract("EPNS", [], "EPNS");
-  // deployedContracts.push(epns)
-
   const EPNSCommV1 = await deployContract("EPNSCommV1", [], "EPNSCommV1");
   deployedContracts.push(EPNSCommV1)
 
   const EPNSAdmin = await deployContract("EPNSAdmin", [], "EPNSAdmin");
   deployedContracts.push(EPNSAdmin)
-  // const timelock = await deployContract("Timelock", [adminSigner.address, delay], "Timelock"); // governor and a guardian,
-  // deployedContracts.push(timelock)
-
-  // const governorAlpha = await deployContract("GovernorAlpha", [
-  //   timelock.address,
-  //   epns.address,
-  //   adminSigner.address
-  // ]
-  // , "GovernorAlpha");
-  // deployedContracts.push(governorAlpha)
-
-  // const currBlock = await ethers.provider.getBlock('latest');
-
-  // const eta = currBlock.timestamp;
-  // const coder = new ethers.utils.AbiCoder();
-
-  // let data = coder.encode(['address'], [governorAlpha.address]);
-
-  // await timelock.functions.queueTransaction(timelock.address, '0', 'setPendingAdmin(address)', data, (eta + 1));
-  // await ethers.provider.send('evm_mine');
-  // await ethers.provider.send('evm_mine');
-  // await timelock.functions.executeTransaction(timelock.address, '0', 'setPendingAdmin(address)', data, (eta + 1));
 
   const EPNSCommProxy = await deployContract("EPNSCommProxy", [
       EPNSCommV1.address,
