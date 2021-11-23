9f9415a912c9a93b6f2621c3b6c242ffb5377a6c
zaryab2000 <zaryabafser2000@gmail.com>
2021-11-23 18:36:24 +0530

    minor changes

--- scripts/versioncontrol/1_deployEPNSCoreV1.config.js
+++ scripts/versioncontrol/1_deployEPNSCoreV1.config.js
@@ -23,13 +23,13 @@ const deploy = {
     }
   },
   args: {
-    daiAddress: "0xf80A32A835F79D7787E8a8ee5721D0fEaFd78108",
-    aDaiAddress: "0xcB1Fe6F440c49E9290c3eb7f158534c2dC374201",
-    wethAddress: "0xc778417E063141139Fce010982780140Aa0cD5Ab",
-    pushAddress: "0xf418588522d5dd018b425E472991E52EBBeEEEEE",
-    uniswapRouterAddress: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
-    aaveLendingAddress: "0x1c8756FD2B28e9426CDBDcC7E3c4d64fa9A54728",
-    referralCode: "0"
+    daiAddress: null,
+    aDaiAddress: null,
+    wethAddress: null,
+    pushAddress: null,
+    uniswapRouterAddress: null,
+    aaveLendingAddress: null,
+    referralCode: null
   }
 }
 
