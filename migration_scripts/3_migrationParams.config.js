84b16193ad00fcdbd1108fa9d27b288dbddb19dc
akp111 <pradhanashish95@gmail.com>
2021-12-08 17:19:08 +0530

    changed abi

--- migration_scripts/3_migrationParams.config.js
+++ migration_scripts/3_migrationParams.config.js
@@ -1,9 +1,9 @@
 const params = {
-  INFURA_PROJECT_ID: "https://kovan.infura.io/v3/19f91487f71a47a49229a12918251929",
-  KEY: "<PVT_KEY>",
+  INFURA_PROJECT_ID: "",
+  KEY: "",
   EPNS_CORE_ADDRESS : "0x263F1105255EcA29DeBf81AfeF298be025aE46c1",
   GAS_SETTINGS : {
-    gasPrice : '1000000000',
+    gasPrice : '10000000000',
     gasLimit : '8500000'
   }
 }
