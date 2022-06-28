b226d4ebab2fdcd53140c73973b39cfb099f1f79
Abishek Bashyal <abishek_bashyal@outlook.com>
2022-06-28 15:18:46 +0545

    fix temp -> 1.5 update slot layouts

--- contracts/EPNSCore/EPNSCoreV1_5.sol
+++ contracts/EPNSCore/EPNSCoreV1_5.sol
@@ -12,6 +12,7 @@ pragma experimental ABIEncoderV2;
  **/
 
 import "./EPNSCoreStorageV1_5.sol";
+import "./EPNSCoreStorageV1_updated.sol";
 import "../interfaces/IPUSH.sol";
 import "../interfaces/IADai.sol";
 import "../interfaces/ILendingPool.sol";
@@ -26,7 +27,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/proxy/Initializable.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
-contract EPNSCoreV1_5 is Initializable, Pausable, EPNSCoreStorageV1_5 {
+contract EPNSCoreV1_5 is Initializable,EPNSCoreStorageV1_updated,Pausable,EPNSCoreStorageV1_5{
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
