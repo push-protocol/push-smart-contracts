c644f406e30242b413eec77ecdd962b7b08b2c50
Abishek Bashyal <abishek_bashyal@outlook.com>
2023-03-17 18:53:24 +0545

    config test done

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-ethers");
-// require("hardhat-gas-reporter");
+require("hardhat-gas-reporter");
 require('hardhat-contract-sizer');
 
 
