e86d27792dc3a389317223f95fc377f773119a13
Zaryab <zaryabafser2000@gmail.com>
2023-03-20 12:24:10 +0530

    removed hardhat console

--- contracts/EPNSCore/EPNSCoreV2.sol
+++ contracts/EPNSCore/EPNSCoreV2.sol
@@ -23,8 +23,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 
-import "hardhat/console.sol";
-
 
 contract EPNSCoreV2 is
     Initializable,
