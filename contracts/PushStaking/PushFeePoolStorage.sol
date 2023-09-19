8d0df58470db6b448d92983a7e21544a70a71209
Zaryab <zaryabafser2000@gmail.com>
2023-09-19 10:19:59 +0400

    removed unwanted state variables

--- contracts/PushStaking/PushFeePoolStorage.sol
+++ contracts/PushStaking/PushFeePoolStorage.sol
@@ -1,15 +1,7 @@
 pragma solidity >=0.6.0 <0.7.0;
 
 contract PushFeePoolStorage {
-    /* *** V2 State variables *** */
-    bytes32 public constant DOMAIN_TYPEHASH =
-        keccak256(
-            "EIP712Domain(string name, uint256 chainId, address verifyingContract)"
-        );
-    bytes32 public constant CREATE_CHANNEL_TYPEHASH =
-        keccak256(
-            "CreateChannel(ChannelType channelType, bytes identity, uint256 amount, uint256 channelExpiryTime, uint256 nonce, uint256 expiry)"
-        );
+
     /** Staking V2 state variables **/
     mapping(address => uint256) public usersRewardsClaimed;
 
