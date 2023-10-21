2de3437d8960ce5a4f49746983b616fee210aa15
Zaryab <zaryabafser2000@gmail.com>
2023-10-21 19:13:40 +0400

    completed baseTest Contract(first iteration)

--- testFoundry/BaseCoreTest.t.sol
+++ testFoundry/BaseCoreTest.t.sol
@@ -2,6 +2,11 @@ pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
 import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import "contracts/token/EPNS.sol";
 import "contracts/PushCore/PushCoreStorageV2.sol";
 import "contracts/interfaces/IUniswapV2Router.sol";
@@ -9,53 +14,49 @@ import "contracts/PushCore/PushCoreStorageV1_5.sol";
 import {PushCoreV2} from "contracts/PushCore/PushCoreV2.sol";
 import {PushCommV2} from "contracts/PushComm/PushCommV2.sol";
 
+import {Actors} from "./utils/Actors.sol";
+import {Constants} from "./utils/Constants.sol";
 
-contract PushTest is Test {
-    /* ***************
-        Main Push Contracts and Tokens
-     *************** */
-     enum ChannelType {
-        ProtocolNonInterest,
-        ProtocolPromotion,
-        InterestBearingOpen,
-        InterestBearingMutual,
-        TimeBound,
-        TokenGaited
-    }
-    enum ChannelAction {
-        ChannelRemoved,
-        ChannelAdded,
-        ChannelUpdated
-    }
+
+contract PushTest is Test, Constants{
+
     EPNS public pushToken;
     PushCoreV2 public core;
-    //PushCommV2 public comm;
+    PushCommV2 public comm;
     IUniswapV2Router public uniV2Router;
 
     /* ***************
         Main Actors in Test
      *************** */
-    address admin = makeAddr("admin");
-    address tony = makeAddr("tony");
-    address bob = makeAddr("bob");
-    address alice = makeAddr("alice");
-    address charlie = makeAddr("charlie");
-
-    address[] users = [bob, alice, charlie, tony];
+     Actors internal actor;
+     address tokenDistributor;
+     
 
     /* ***************
        Initializing Set-Up for Push Contracts
      *************** */
 
     function setUp() external {
-        
-        pushToken = new EPNS(admin);
+        tokenDistributor = makeAddr("tokenDistributor");
+
+        pushToken = new EPNS(tokenDistributor);
         core = new PushCoreV2();
         comm = new PushCommV2();
         uniV2Router = IUniswapV2Router(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+
+        actor = Actors({
+            admin: createActor("admin"),
+            governance: createActor("governance"),
+            bob_channel_owner: createActor("bob_channel_owner"),
+            alice_channel_owner: createActor("alice_channel_owner"),
+            charlie_channel_owner: createActor("charlie_channel_owner"),
+            dan_push_holder: createActor("dan_push_holder"),
+            tim_push_holder: createActor("tim_push_holder")
+        });
+
         // Initialize Core Contract
         core.initialize(
-            admin,
+            actor.admin,
             address(pushToken),
             address(0), // WETH Address
             address(uniV2Router), // Uniswap_Router
@@ -64,57 +65,26 @@ contract PushTest is Test {
             address(0), // aDai address
             0
         );
-        // Transfer 100K Push To all actors
 
-        uint _amount = 100000 * 1e18;
+        // Initialize Comm Contract
+        comm.initialize(
+            actor.admin,
+            "FOUNDRY_TEST_NETWORK"
+        );
 
-        
-        for (uint i = 0; i < users.length; i++) {
-            vm.prank(admin);
-            pushToken.transfer(users[i], _amount);
-        }
-        for (uint i; i < users.length; ++i) {
-            approveTokens(users[i], address(core), _amount);
-        }
-
-        // Set Approval for Admin and Holder Delegation
-        vm.startPrank(admin);
-        core.setMinPoolContribution(1 ether); // as per the change in v1.5.0
-        pushToken.approve(address(core), 100000000 * 1e18);
-        pushToken.setHolderDelegation(address(core), true);
-    
+        // Set-up Core Address in Comm & Vice-Versa
+        vm.startPrank(actor.admin);
+        comm.setEPNSCoreAddress(address(core));
+        core.setEpnsCommunicatorAddress(address(comm));
         vm.stopPrank();
-    }
-
-      /* ***************
-       Basic tests in Core Contract
-      *************** */
-
-    // 1. Admin should be accurate
-    function testCoreAdmin() public {
-        address coreAdmin = core.pushChannelAdmin();
-        assertEq(coreAdmin, admin);
-    }
-    // 2. Fee-Related States should be accurate
-    function testFeeRelatedStates() public {
-        uint256 feeAmount = core.FEE_AMOUNT();
-        uint256 minChannelFees = core.ADD_CHANNEL_MIN_FEES();
-        uint256 minContribution = core.MIN_POOL_CONTRIBUTION();
-        
-        assertEq(feeAmount, 10 ether);
-        assertEq(minChannelFees, 50 ether);
-        assertEq(minContribution, 1 ether);
-    }
-    // 3. Only Owner functions are protected
-    function testFailOwnerAccess() public {
-        vm.prank(bob);
-        core.pauseContract();
+        // Wrapping to exact timestamp of Core and Comm Deployment
+        vm.warp(DEC_27_2021);
     }
 
     /* ***************
        Core Contract Helper Functions
     *************** */
-     
+
     function roll(uint num) internal {
         vm.roll(num);
     }
@@ -126,35 +96,16 @@ contract PushTest is Test {
         vm.stopPrank();
     }
 
-    // Staking-Related Functions
-    function stake(address signer, uint256 amount) internal {
-        vm.prank(signer);
-        core.stake(amount * 1e18);
-    }
-
-    function harvest(address signer) internal {
-        vm.prank(signer);
-        core.harvestAll();
-    }
-
-    function harvestPaginated(address signer, uint _till) internal {
-        vm.prank(signer);
-        core.harvestPaginated(_till);
-    }
-
-    function addPool(uint256 amount) internal {
-        vm.prank(admin);
-        core.addPoolFees(amount);
-    }
-
-    function unstake(address signer) internal {
-        vm.prank(signer);
-        core.unstake();
-    }
-
-    function daoHarvest(uint _epoch) internal {
-        vm.prank(admin);
-        core.daoHarvestPaginated(_epoch);
+    function createActor(string memory name) internal returns(address payable){
+        address payable actor = payable(makeAddr(name));
+        // Transfer 50 eth to every actor
+        vm.deal({ account: actor, newBalance: 50 ether });
+        // Transfer 50K PUSH Tokens for every actor
+        vm.prank(tokenDistributor);
+        pushToken.transfer(actor, 50_000 ether);
+        // Approve tokens for Core Contract 
+        approveTokens(actor, address(core), 50_000 ether);
+        return actor;
     }
 
 }
---
+++ testFoundry/utils/Actors.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.6.11;
+
+struct Actors {
+    // Default admin for all Push Contracts
+    address payable admin;
+    // Default governance addr for all Push Contracts
+    address payable governance;
+    // Channel Owner Named - Bob
+    address payable bob_channel_owner;
+    // Channel Owner Named - Alice
+    address payable alice_channel_owner;
+    // Channel Owner Named - Charlie 
+    address payable charlie_channel_owner;
+    // Push Token Holder - Dan
+    address payable dan_push_holder;
+    // Push Token Holder - tim
+    address payable tim_push_holder;
+}
