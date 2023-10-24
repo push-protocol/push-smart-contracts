0981d24cc355e2f2136f100504629c2e8edeb118
Zaryab <zaryabafser2000@gmail.com>
2023-10-24 12:39:32 +0400

    added more constants for testing

--- testFoundry/utils/Constants.sol
+++ testFoundry/utils/Constants.sol
@@ -2,5 +2,15 @@
 pragma solidity 0.6.11;
 
 abstract contract Constants {
+    // General Constant Values of All Contracts
     uint256 internal constant DEC_27_2021 = 1_640_605_391;
+
+
+    // Specific Constant Values for Staking-Related Contracts 
+    uint256 public genesisEpoch = 17821509;
+    uint256 public lastEpochInitialized = 5;
+    uint256 public lastTotalStakeEpochInitialized = 0;
+    uint256 public totalStakedAmount = 6654086 ether;
+    uint256 public previouslySetEpochRewards = 60000 ether;
+    uint256 public constant epochDuration = 21 * 7156; // 21 * number of blocks per day(7156) ~ 20 day approx
 }
