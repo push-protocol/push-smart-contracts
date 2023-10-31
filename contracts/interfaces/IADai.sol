81461a9c670aba08c89f5a6c1f8d1b69b7a779b6
Zaryab <zaryabafser2000@gmail.com>
2023-10-31 12:08:01 +0400

    removed unwanted interfaces

--- contracts/interfaces/IADai.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.20;
-
-interface IADai {
-    function redeem(uint256 _amount) external;
-    function balanceOf(address _user) external view returns (uint256);
-    function principalBalanceOf(address _user) external view returns (uint256);
-    function getInterestRedirectionAddress(address _user) external view returns (address);
-}
--- contracts/interfaces/ITokenBridge.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.8.20;
-
-interface ITokenBridge {
-    function completeTransferWithPayload(bytes memory encodedVm) external returns (bytes memory);
-}
