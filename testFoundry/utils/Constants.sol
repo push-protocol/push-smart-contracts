3c8b3e0a6adac7facde0f0961e05314ecf9fd50b
zartaj <zartaj.afsar@gmail.com>
2023-11-23 11:50:37 +0530

    changed constants for testing

--- testFoundry/utils/Constants.sol
+++ testFoundry/utils/Constants.sol
@@ -6,10 +6,17 @@ abstract contract Constants {
     uint256 internal constant DEC_27_2021 = 1_640_605_391;
 
     // Specific Constant Values for Staking-Related Contracts
-    uint256 public genesisEpoch = 17_821_509;
-    uint256 public lastEpochInitialized = 5;
+    // uint256 public genesisEpoch = 17_821_509;
+    // uint256 public lastEpochInitialized = 5;
+    // uint256 public lastTotalStakeEpochInitialized = 0;
+    // uint256 public totalStakedAmount = 6_654_086 ether;
+    // uint256 public previouslySetEpochRewards = 60_000 ether;
+    // uint256 public constant epochDuration = 21 * 7156; // 21 * number of blocks per day(7156) ~ 20 day approx
+
+    uint256 public genesisEpoch = block.number;
+    uint256 public lastEpochInitialized = genesisEpoch;
     uint256 public lastTotalStakeEpochInitialized = 0;
-    uint256 public totalStakedAmount = 6_654_086 ether;
-    uint256 public previouslySetEpochRewards = 60_000 ether;
+    uint256 public totalStakedAmount = 0 ether;
+    uint256 public previouslySetEpochRewards = 0 ether;
     uint256 public constant epochDuration = 21 * 7156; // 21 * number of blocks per day(7156) ~ 20 day approx
 }
