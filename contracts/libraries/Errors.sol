78f603cefb8930be45a316929764ac1008f92109
zartaj <zartaj.afsar@gmail.com>
2023-11-28 15:54:45 +0530

    Add separate Errors file

---
+++ contracts/libraries/Errors.sol
@@ -0,0 +1,13 @@
+// // SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// pragma solidity ^0.8.20;
+
+error InvalidCallerParam(string err);
+error InvalidCaller();
+error InvalidChannel();
+error InvalidArgument(string err);
+error InvalidAmount();
+error InvalidLogic(string err);
+error InvalidEpoch(string err);
+
+error InvalidSignature(string err);
+error InvalidSubscriber(string err);
