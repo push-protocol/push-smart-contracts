657da6c88f1170e793fcb0988204668eb53c3329
Nilesh Gupta <guptanilesh2312@gmail.com>
2023-12-20 16:38:19 +0530

    Added unit tests for contract pausability

--- testFoundry/utils/Events.sol
+++ testFoundry/utils/Events.sol
@@ -1,10 +1,19 @@
 pragma solidity ^0.8.20;
 import {PushCoreStorageV1_5} from "contracts/PushCore/PushCoreStorageV1_5.sol";
 
-
 abstract contract CoreEvents {
     event AddChannel(address indexed channel, PushCoreStorageV1_5.ChannelType indexed channelType, bytes identity);
+    event UpdateChannel(address indexed channel, bytes identity, uint256 indexed amountDeposited);
     event ReactivateChannel(address indexed channel, uint256 indexed amountDeposited);
     event DeactivateChannel(address indexed channel, uint256 indexed amountRefunded);
     event ChannelBlocked(address indexed channel);
 }
+
+abstract contract CommEvents {}
+
+abstract contract ProxyEvents {
+    event Paused(address account);
+    event Unpaused(address account);
+}
+
+abstract contract Events is CoreEvents, CommEvents, ProxyEvents {}
