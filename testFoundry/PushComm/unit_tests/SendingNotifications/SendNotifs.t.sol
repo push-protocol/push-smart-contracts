3bb3bf45d354d5ba551f582ba02d45a71a48d711
Md Zartaj Afser <zartaj.afsar@gmail.com>
2023-12-21 15:03:50 +0530

    add test for sendNotification

---
+++ testFoundry/PushComm/unit_tests/SendingNotifications/SendNotifs.t.sol
@@ -0,0 +1,95 @@
+pragma solidity ^0.8.0;
+import {BaseTest} from "../../../BaseTest.t.sol";
+import {PushCoreStorageV1_5} from "contracts/PushCore/PushCoreStorageV1_5.sol";
+
+// import "forge-std/console.sol";
+contract SendNotifs_Test is BaseTest {
+    bytes constant _testChannelIdentity = bytes("test-channel-hello-world");
+
+    function setUp() public override {
+        BaseTest.setUp();
+        changePrank(actor.bob_channel_owner);
+        coreProxy.createChannelWithPUSH(
+            PushCoreStorageV1_5.ChannelType.InterestBearingOpen,
+            _testChannelIdentity,
+            50e18,
+            0
+        );
+    }
+
+    function test_WhenAUserIsSendingNotifToOtherAddressInsteadOfThemselves()
+        external
+    {
+        changePrank(actor.charlie_channel_owner);
+        bool res = commProxy.sendNotification(
+            actor.bob_channel_owner,
+            actor.alice_channel_owner,
+            _testChannelIdentity
+        );
+        assertEq(res, false);
+    }
+
+    function test_WhenRecipientIsSendingNOTIFOnlyToThemselves() external {
+        //User can no longer send notifs to themselves
+    }
+
+    function test_WhenChannelIs0x00ButCallerIsAnyAddressOtherThanAdminOrGovernance()
+        external
+    {
+        changePrank(actor.charlie_channel_owner);
+        bool res = commProxy.sendNotification(
+            address(0),
+            actor.alice_channel_owner,
+            _testChannelIdentity
+        );
+        assertEq(res, false);
+    }
+
+    function test_WhenChannelIs0x00AndCallerIsAdminOrGovernance() external {
+        changePrank(actor.admin);
+        bool res = commProxy.sendNotification(
+            address(0),
+            actor.alice_channel_owner,
+            _testChannelIdentity
+        );
+        assertEq(res, true);
+    }
+
+    function test_WhenDelegateSendNotificationWithoutApproval() external {
+        bool isTonyDelegate = commProxy.delegatedNotificationSenders(
+            actor.bob_channel_owner,
+            actor.dan_push_holder
+        );
+
+        assertEq(isTonyDelegate, false);
+
+        changePrank(actor.dan_push_holder);
+        bool res = commProxy.sendNotification(
+            actor.bob_channel_owner,
+            actor.alice_channel_owner,
+            _testChannelIdentity
+        );
+        assertEq(res, false);
+    }
+
+    function test_WhenAllowedDelagtesSendsNotificationToAnyRecipient()
+        external
+    {
+        changePrank(actor.bob_channel_owner);
+        commProxy.addDelegate(actor.dan_push_holder);
+        bool isTonyDelegate = commProxy.delegatedNotificationSenders(
+            actor.bob_channel_owner,
+            actor.dan_push_holder
+        );
+
+        assertEq(isTonyDelegate, true);
+
+        changePrank(actor.dan_push_holder);
+        bool res = commProxy.sendNotification(
+            actor.bob_channel_owner,
+            actor.alice_channel_owner,
+            _testChannelIdentity
+        );
+        assertEq(res, true);
+    }
+}
