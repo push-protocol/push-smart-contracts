0121572250e0071fa4ae2af50ad00a3de9e69df1
Md Zartaj Afser <zartaj.afsar@gmail.com>
2024-01-19 11:51:25 +0530

    removed getChainId

--- contracts/libraries/BaseHelper.sol
+++ contracts/libraries/BaseHelper.sol
@@ -4,14 +4,6 @@ pragma solidity ^0.8.20;
 /// @title CommHelper
 /// @notice Library with helper functions needed for the Push Communicator Contract
 library BaseHelper {
-    function getChainId() internal view returns (uint256) {
-        uint256 chainId;
-        assembly {
-            chainId := chainid()
-        }
-        return chainId;
-    }
-
     function isContract(address account) internal view returns (bool) {
         // This method relies on extcodesize, which returns 0 for contracts in
         // construction, since the code is only stored at the end of the
