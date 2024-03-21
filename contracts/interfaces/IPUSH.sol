fc7a7d5f19117c80c161cd8f737b6139520ba63c
Zaryab <zaryabafser2000@gmail.com>
2024-03-21 12:55:19 +0400

    added gov-related funcs

--- contracts/interfaces/IPUSH.sol
+++ contracts/interfaces/IPUSH.sol
@@ -5,5 +5,10 @@ interface IPUSH {
     function totalSupply() external view returns (uint256);
     function resetHolderWeight(address holder) external;
     function holderWeight(address) external view returns (uint256);
+    function approve(address spender, uint256 rawAmount) external returns (bool);
     function returnHolderUnits(address account, uint256 atBlock) external view returns (uint256);
+
+    //Voting and Governance Related Function Signatures
+    function delegate(address delegatee) external;
+
 }
