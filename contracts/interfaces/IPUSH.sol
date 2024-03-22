1b5912e1ad0162765375e7e5ebab63cc8b42d6ab
Zaryab <zaryabafser2000@gmail.com>
2024-03-22 09:29:28 +0400

    updated Push Interface

--- contracts/interfaces/IPUSH.sol
+++ contracts/interfaces/IPUSH.sol
@@ -1,14 +1,21 @@
 pragma solidity ^0.8.20;
 
 interface IPUSH {
-    function born() external view returns (uint256);
+    function decimals() external view returns (uint8);
     function totalSupply() external view returns (uint256);
+    function symbol() external view returns (string memory);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address dst, uint256 rawAmount) external returns (bool);
+    function approve(address spender, uint256 rawAmount) external returns (bool);
+    function allowance(address account, address spender) external view returns (uint256);
+    function transferFrom(address src, address dst, uint256 rawAmount) external returns (bool);
+    
+    // Push-Token Specific Functions
+    function born() external view returns (uint256);
     function resetHolderWeight(address holder) external;
     function holderWeight(address) external view returns (uint256);
-    function approve(address spender, uint256 rawAmount) external returns (bool);
     function returnHolderUnits(address account, uint256 atBlock) external view returns (uint256);
 
     //Voting and Governance Related Function Signatures
     function delegate(address delegatee) external;
-
 }
