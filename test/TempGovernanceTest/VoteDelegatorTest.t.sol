e7404d880d3130d801d1495c1c733f0f3ca35cb3
Zaryab <zaryabafser2000@gmail.com>
2024-03-22 09:26:09 +0400

    test voteHolderContract

--- test/TempGovernanceTest/VoteDelegatorTest.t.sol
+++ test/TempGovernanceTest/VoteDelegatorTest.t.sol
@@ -20,15 +20,15 @@ contract PushVoteDelegatorTest is Test, BaseTest{
    vm.assume(_deployer != address(0));
 
     vm.prank(_deployer);
-    PushVoteDelegator _surrogate = new PushVoteDelegator(IPUSH(address(govToken)), _delegatee);
-    return _surrogate;
+    PushVoteDelegator _voteDelegator = new PushVoteDelegator(IPUSH(address(govToken)), _delegatee);
+    return _voteDelegator;
   }
 }
 
 contract Constructor is PushVoteDelegatorTest {
   function testFuzz_DelegatesToDeployer(address _deployer, address _delegatee) public {
-    PushVoteDelegator _surrogate = __deploy(_deployer, _delegatee);
-    assertEq(_delegatee, govToken.delegates(address(_surrogate)));
+    PushVoteDelegator _voteDelegator = __deploy(_deployer, _delegatee);
+    assertEq(_delegatee, govToken.delegates(address(_voteDelegator)));
   }
 
   function testFuzz_MaxApprovesDeployerToEnableWithdrawals(
@@ -39,24 +39,24 @@ contract Constructor is PushVoteDelegatorTest {
   ) public {
     vm.assume(_receiver != address(0));
 
-    PushVoteDelegator _surrogate = __deploy(_deployer, _delegatee);
-    govToken.mint(address(_surrogate), _amount);
+    PushVoteDelegator _voteDelegator = __deploy(_deployer, _delegatee);
+    govToken.mint(address(_voteDelegator), _amount);
 
-    uint256 _allowance = govToken.allowance(address(_surrogate), _deployer);
+    uint256 _allowance = govToken.allowance(address(_voteDelegator), _deployer);
     assertEq(_allowance, type(uint96).max);
 
     vm.prank(_deployer);
-    //govToken.transferFrom(address(_surrogate), _receiver, _amount);
+    //govToken.transferFrom(address(_voteDelegator), _receiver, _amount);
 
     //assertEq(govToken.balanceOf(_receiver), _amount);
   }
 
   function test_DelegationWorks_with_EOA() public {
     address _delegatee = actor.bob_channel_owner; 
-    PushVoteDelegator _surrogate = __deploy(actor.admin, _delegatee);
-    govToken.mint(address(_surrogate), 1000);
+    PushVoteDelegator _voteDelegator = __deploy(actor.admin, _delegatee);
+    govToken.mint(address(_voteDelegator), 1000);
 
-    uint256 balSurrogate = govToken.balanceOf(address(_surrogate));
+    uint256 balSurrogate = govToken.balanceOf(address(_voteDelegator));
     // Balance of Surrogate is right
     assertEq(balSurrogate, 1000);
 
@@ -91,18 +91,18 @@ contract Constructor is PushVoteDelegatorTest {
 
     // 2. Alice deploys her VoteHolder Contract and delegates to Charlie
     vm.startPrank(actor.alice_channel_owner);
-    PushVoteDelegator _surrogate = new PushVoteDelegator(IPUSH(address(govToken)), actor.charlie_channel_owner);
+    PushVoteDelegator _voteDelegator = new PushVoteDelegator(IPUSH(address(govToken)), actor.charlie_channel_owner);
     
     uint96 voteCountCharlie_before = govToken.getCurrentVotes(actor.charlie_channel_owner);
     // 3. After that, she transfers 1129 Token into VoteHolder Contract
-    govToken.transfer(address(_surrogate), 1129);
-    uint256 balSurrogate = govToken.balanceOf(address(_surrogate));
+    govToken.transfer(address(_voteDelegator), 1129);
+    uint256 balSurrogate = govToken.balanceOf(address(_voteDelegator));
     assertEq(balSurrogate, 1129);
     
     // 4. Checks:
     //  a. Delegates state should be marked correctly - VoteHolder => Charlie
     //  b. Charlie should have votes
-    address delegateState = govToken.delegates(address(_surrogate));
+    address delegateState = govToken.delegates(address(_voteDelegator));
     uint96 voteCountCharlie_after = govToken.getCurrentVotes(actor.charlie_channel_owner);
 
     assertEq(delegateState, actor.charlie_channel_owner);
@@ -110,7 +110,7 @@ contract Constructor is PushVoteDelegatorTest {
     assertEq(voteCountCharlie_after, 1129);
 
     // 5. Then Alice transfers 1000 More tokens to VoteHolder
-    govToken.transfer(address(_surrogate), 1000);
+    govToken.transfer(address(_voteDelegator), 1000);
     uint96 voteCountCharlie_after2nd = govToken.getCurrentVotes(actor.charlie_channel_owner);
 
     assertEq(voteCountCharlie_after2nd, 2129);
