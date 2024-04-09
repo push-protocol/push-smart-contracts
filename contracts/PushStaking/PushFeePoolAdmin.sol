b03e625b6150e7824dbb6a6c0f80f87e1a57a6fe
Zaryab <zaryabafser2000@gmail.com>
2024-04-09 17:11:44 +0400

    removed unwanted files

--- contracts/PushStaking/PushFeePoolAdmin.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.20;
-
-import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
-
-contract PushFeePoolAdmin is ProxyAdmin {
-    constructor(address _pushChannelAdmin) public ProxyAdmin(_pushChannelAdmin) { }
-}
