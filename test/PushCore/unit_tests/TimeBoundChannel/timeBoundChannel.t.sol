fd1ea7cab7ee32f4780dd6e0e1c29bbe0b487319
Zaryab <zaryabafser2000@gmail.com>
2024-04-11 11:15:36 +0400

    update timeBound tests

--- test/PushCore/unit_tests/TimeBoundChannel/timeBoundChannel.t.sol
+++ test/PushCore/unit_tests/TimeBoundChannel/timeBoundChannel.t.sol
@@ -44,15 +44,6 @@ contract TimeBoundChannel_Test is BasePushCoreTest {
         assertEq(expectedExpiryTime, actualExpiryTime);
     }
 
-    // function test_Revertwhen_DestroyNotTimeBoundChannel() public whenNotPaused {
-    //     _createChannel(actor.bob_channel_owner);
-
-    //     vm.expectRevert(abi.encodeWithSelector(Errors.Core_InvalidChannelType.selector));
-
-    //     vm.prank(actor.bob_channel_owner);
-    //     coreProxy.updateChannelState(0);
-    // }
-
     function test_Revertwhen_DestroyByOtherAfterExpiry() public whenNotPaused {
         _createTimeBoundChannel(actor.bob_channel_owner, _getFutureTime(1 days));
 
@@ -86,39 +77,6 @@ contract TimeBoundChannel_Test is BasePushCoreTest {
         coreProxy.updateChannelState(0);
     }
 
-    // function test_Revertwhen_DestroyByAdminBeforeExpiry() public whenNotPaused {
-    //     _createTimeBoundChannel(actor.bob_channel_owner, _getFutureTime(2 days));
-
-    //     skip(1 days);
-
-    //     vm.expectRevert(abi.encodeWithSelector(Errors.UnauthorizedCaller.selector, actor.admin));
-
-    //     vm.prank(actor.bob_channel_owner);
-    //     coreProxy.updateChannelState(0);
-    // }
-
-    // function test_Revertwhen_DestroyByAdminAfterExpiryBefore14Days() public whenNotPaused {
-    //     _createTimeBoundChannel(actor.bob_channel_owner, _getFutureTime(1 days));
-
-    //     skip(2 days);
-
-    //     vm.expectRevert(abi.encodeWithSelector(Errors.UnauthorizedCaller.selector, actor.admin));
-
-    //     vm.prank(actor.bob_channel_owner);
-    //     coreProxy.updateChannelState(0);
-    // }
-
-    // function test_Revertwhen_DestroyByAdminAfterExpiryEqualTo14Days() public whenNotPaused {
-    //     _createTimeBoundChannel(actor.bob_channel_owner, _getFutureTime(1 days));
-
-    //     skip(15 days);
-
-    //     vm.expectRevert(abi.encodeWithSelector(Errors.UnauthorizedCaller.selector, actor.admin));
-
-    //     vm.prank(actor.bob_channel_owner);
-    //     coreProxy.updateChannelState(0);
-    // }
-
     function test_Revert_when_DestroyByOwnerAtExpiry() public whenNotPaused {
         _createTimeBoundChannel(actor.bob_channel_owner, _getFutureTime(1 days));
 
@@ -142,47 +100,6 @@ contract TimeBoundChannel_Test is BasePushCoreTest {
         vm.prank(actor.bob_channel_owner);
         coreProxy.updateChannelState(0);
     }
-    // Admin can't perform this in the updated function
-    // function test_DestroyByAdminAfterExpiryAfter14Days() public whenNotPaused {
-    //     _createTimeBoundChannel(actor.bob_channel_owner, _getFutureTime(1 days));
-
-    //     skip(15 days + 1 seconds);
-
-    //     uint256 actualPoolContribution = _getChannelPoolContribution(actor.bob_channel_owner);
-
-    //     emit TimeBoundChannelDestroyed(actor.admin, actualPoolContribution);
-
-    //     vm.prank(actor.bob_channel_owner);
-    //     coreProxy.updateChannelState(0);
-    // }
-
-    // function test_VariablesUpdationAfterDestroyedByAdmin() public whenNotPaused {
-    //     _createTimeBoundChannel(actor.bob_channel_owner, _getFutureTime(1 days));
-
-    //     skip(15 days + 1 seconds);
-
-    //     uint256 poolContributionBeforeDestroyed = _getChannelPoolContribution(actor.bob_channel_owner);
-    //     uint256 channelsCountBeforeDestroyed = coreProxy.channelsCount();
-    //     uint256 channelPoolFundsBeforeDestroyed = coreProxy.CHANNEL_POOL_FUNDS();
-    //     uint256 protocolPoolFeesBeforeDestroyed = coreProxy.PROTOCOL_POOL_FEES();
-
-    //     vm.prank(actor.bob_channel_owner);
-    //     coreProxy.updateChannelState(0);
-
-    //     uint256 actualChannelsCountAfterDestroyed = coreProxy.channelsCount();
-    //     uint256 actualChannelPoolFundsAfterDestroyed = coreProxy.CHANNEL_POOL_FUNDS();
-    //     uint256 actualProtocolPoolFeesAfterDestroyed = coreProxy.PROTOCOL_POOL_FEES();
-    //     uint256 expectedChannelsCountAfterDestroyed = channelsCountBeforeDestroyed - 1;
-    //     uint256 expectedChannelPoolFundsAfterDestroyed =
-    //         channelPoolFundsBeforeDestroyed - poolContributionBeforeDestroyed;
-    //     uint256 expectedProtocolPoolFeesAfterDestroyed =
-    //         protocolPoolFeesBeforeDestroyed + poolContributionBeforeDestroyed;
-
-    //     assertEq(expectedChannelsCountAfterDestroyed, actualChannelsCountAfterDestroyed);
-    //     assertEq(expectedChannelPoolFundsAfterDestroyed, actualChannelPoolFundsAfterDestroyed);
-    //     assertEq(expectedProtocolPoolFeesAfterDestroyed, actualProtocolPoolFeesAfterDestroyed);
-    // }
-
     function test_VariablesUpdationAfterDestroyedByOwner() public whenNotPaused {
         _createTimeBoundChannel(actor.bob_channel_owner, _getFutureTime(1 days));
 
@@ -225,25 +142,6 @@ contract TimeBoundChannel_Test is BasePushCoreTest {
 
         assertEq(expectedPushTokenBalanceOfOwnerAfterDestroying, actualPushTokenBalanceOfOwnerAfterDestroying);
     }
-
-    // function test_ShouldNotRefundAfterDestroyedByAdmin() public whenNotPaused {
-    //     _createTimeBoundChannel(actor.bob_channel_owner, _getFutureTime(1 days));
-
-    //     skip(15 days + 1 seconds);
-    //     uint256 pushTokenBalanceOfOwnerBeforeDestroying = pushToken.balanceOf(actor.bob_channel_owner);
-    //     uint256 pushTokenBalanceOfAdminBeforeDestroying = pushToken.balanceOf(actor.admin);
-
-    //     vm.prank(actor.bob_channel_owner);
-    //     coreProxy.updateChannelState(0);
-
-    //     uint256 actualPushTokenBalanceOfOwnerAfterDestroying = pushToken.balanceOf(actor.bob_channel_owner);
-    //     uint256 expectedPushTokenBalanceOfOwnerAfterDestroying = pushTokenBalanceOfOwnerBeforeDestroying;
-    //     uint256 actualPushTokenBalanceOfAdminAfterDestroying = pushToken.balanceOf(actor.admin);
-    //     uint256 expectedPushTokenBalanceOfAdminAfterDestroying = pushTokenBalanceOfAdminBeforeDestroying;
-
-    //     assertEq(expectedPushTokenBalanceOfOwnerAfterDestroying, actualPushTokenBalanceOfOwnerAfterDestroying);
-    //     assertEq(expectedPushTokenBalanceOfAdminAfterDestroying, actualPushTokenBalanceOfAdminAfterDestroying);
-    // }
 
     function test_ShouldDeleteDataAfterDestroyed() public whenNotPaused {
         _createTimeBoundChannel(actor.bob_channel_owner, _getFutureTime(1 days));
@@ -304,34 +202,4 @@ contract TimeBoundChannel_Test is BasePushCoreTest {
         vm.prank(actor.bob_channel_owner);
         coreProxy.updateChannelState(0);
     }
-
-    // Auto-UnSubscription from Channels - Now Deprecated
-
-    // function test_ShouldUnsubscribeAfterDestroyed() public whenNotPaused {
-    //     _createTimeBoundChannel(actor.bob_channel_owner, _getFutureTime(1 days));
-    //     address EPNS_ALERTER = address(0);
-
-    //     bool isChannelSubscribedToOwn_Before =
-    //         commProxy.isUserSubscribed(actor.bob_channel_owner, actor.bob_channel_owner);
-    //     bool isChannelSubscribedToEPNS_Before = commProxy.isUserSubscribed(EPNS_ALERTER, actor.bob_channel_owner);
-    //     bool isAdminSubscribedToChannel_Before = commProxy.isUserSubscribed(actor.bob_channel_owner, actor.admin);
-
-    //     skip(1 days + 1 seconds);
-
-    //     vm.prank(actor.bob_channel_owner);
-    //     coreProxy.updateChannelState(0);
-
-    //     bool isChannelSubscribedToOwn_After =
-    //         commProxy.isUserSubscribed(actor.bob_channel_owner, actor.bob_channel_owner);
-    //     bool isChannelSubscribedToEPNS_After = commProxy.isUserSubscribed(EPNS_ALERTER, actor.bob_channel_owner);
-    //     bool isAdminSubscribedToChannel_After = commProxy.isUserSubscribed(actor.bob_channel_owner, actor.admin);
-
-    //     assertEq(isChannelSubscribedToOwn_Before, true);
-    //     assertEq(isChannelSubscribedToEPNS_Before, true);
-    //     assertEq(isAdminSubscribedToChannel_Before, true);
-
-    //     assertEq(isChannelSubscribedToOwn_After, false);
-    //     assertEq(isChannelSubscribedToEPNS_After, false);
-    //     assertEq(isAdminSubscribedToChannel_After, false);
-    // }
 }
