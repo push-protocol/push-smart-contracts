d0758622da8650bcb2893407a68022d8c070673f
Zaryab <zaryabafser2000@gmail.com>
2024-04-11 13:31:37 +0400

    included NTT Wrapped Push Token

--- contracts/interfaces/IPUSH.sol
+++ contracts/interfaces/IPUSH.sol
@@ -6,4 +6,22 @@ interface IPUSH {
     function resetHolderWeight(address holder) external;
     function holderWeight(address) external view returns (uint256);
     function returnHolderUnits(address account, uint256 atBlock) external view returns (uint256);
+
+    // ----------- Additional Functions for NTT Support ------------- //
+
+    // NOTE: the `mint` method is not present in the standard ERC20 interface.
+    /// @notice Mints `_amount` tokens to `_account`, only callable by the minter.
+    /// @param account The address to receive the minted tokens.
+    /// @param amount The amount of tokens to mint.
+    function mint(address account, uint256 amount) external;
+
+    // NOTE: the `setMinter` method is not present in the standard ERC20 interface.
+    /// @notice Sets a new minter address, only callable by the contract owner.
+    /// @param newMinter The address of the new minter.
+    function setMinter(address newMinter) external;
+
+    // NOTE: NttTokens in `burn` mode require the `burn` method to be present.
+    //       This method is not present in the standard ERC20 interface, but is
+    //       found in the `ERC20Burnable` interface.
+    function burn(uint256 amount) external;
 }
