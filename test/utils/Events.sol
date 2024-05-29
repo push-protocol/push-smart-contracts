aff48cf7c22b525c75d27af72279ea0005dbb3d5
Zaryab <zaryabafser2000@gmail.com>
2024-05-29 18:57:15 +0400

    updateChannelMeta tests modified for bytes32

--- test/utils/Events.sol
+++ test/utils/Events.sol
@@ -5,7 +5,7 @@ import { CoreTypes } from "../../contracts/libraries/DataTypes.sol";
 abstract contract CoreEvents {
     event AddChannel(address indexed channel, CoreTypes.ChannelType indexed channelType, bytes identity);
     event ChannelCreated(bytes32 indexed channel, CoreTypes.ChannelType indexed channelType, bytes identity);
-    event UpdateChannel(address indexed channel, bytes identity, uint256 indexed amountDeposited);
+    event UpdateChannel(bytes32 indexed channel, bytes identity, uint256 indexed amountDeposited);
     event ReactivateChannel(address indexed channel, uint256 indexed amountDeposited);
     event DeactivateChannel(address indexed channel, uint256 indexed amountRefunded);
     event ChannelBlocked(address indexed channel);
