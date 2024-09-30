8bd9f1014956d0ae124e40bfe9f3df7018d60c15
Md Zartaj Afser <zartaj.afsar@gmail.com>
2024-09-30 16:22:27 +0530

    changes in core/removal of staking

--- contracts/PushCore/PushCoreStorageV1_5.sol
+++ contracts/PushCore/PushCoreStorageV1_5.sol
@@ -20,7 +20,7 @@ contract PushCoreStorageV1_5 {
 
     address public pushChannelAdmin;
     address public governance;
-    address public daiAddress;
+    address public STAKING_CONTRACT; //TODO Re-Using Dai's address slot
     address public aDaiAddress;
     address public WETH_ADDRESS;
     address public pushCommunicator;
@@ -33,9 +33,9 @@ contract PushCoreStorageV1_5 {
     uint256 public channelsCount;
 
     ///  @notice Helper Variables for FSRatio Calculation | GROUPS = CHANNELS -> NOT IN USE
-    uint256 public groupNormalizedWeight;
-    uint256 public groupHistoricalZ;
-    uint256 public groupLastUpdate;
+    uint256 public HOLDER_FEE_POOL;//TODO Re-Using groupNormalizedWeight slot
+    uint256 public WALLET_FEE_POOL;   //TODO Re-Using groupHistoricalZ slot
+    uint256 public SPLIT_PERCENTAGE_FOR_HOLDER; //TODO Re-Using groupLastUpdate slot
     uint256 public groupFairShareCount;
 
     /// @notice Necessary variables for Keeping track of Funds and Fees
