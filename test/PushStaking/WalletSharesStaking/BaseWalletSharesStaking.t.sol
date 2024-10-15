3a47493aedb091a92c8be1e918d95c6710276157
Nilesh Gupta <guptanilesh2312@gmail.com>
2024-10-15 17:15:43 +0530

    refactor: added share invariants

--- test/PushStaking/WalletSharesStaking/BaseWalletSharesStaking.t.sol
+++ test/PushStaking/WalletSharesStaking/BaseWalletSharesStaking.t.sol
@@ -8,4 +8,39 @@ contract BaseWalletSharesStaking is BasePushStaking {
     function setUp() public virtual override {
         BasePushStaking.setUp();
     }
+
+    modifier validateShareInvariants () {
+        uint256 walletSharesBeforeExecution = pushStaking.WALLET_TOTAL_SHARES();
+        _;
+        _validateWalletSharesSum();
+        _validateEpochShares();
+        _verifyTotalSharesConsistency(walletSharesBeforeExecution);
+    }
+
+    function _validateWalletSharesSum() internal {
+        uint256 walletTotalShares = pushStaking.WALLET_TOTAL_SHARES();
+        (uint256 foundationWalletShares,,) = pushStaking.walletShareInfo(actor.admin);
+        (uint256 bobWalletShares,,) = pushStaking.walletShareInfo(actor.bob_channel_owner);
+        (uint256 aliceWalletShares,,) = pushStaking.walletShareInfo(actor.alice_channel_owner);
+        (uint256 charlieWalletShares,,) = pushStaking.walletShareInfo(actor.charlie_channel_owner);
+        (uint256 tonyWalletShares,,) = pushStaking.walletShareInfo(actor.tony_channel_owner);
+
+        uint256 totalSharesSum = foundationWalletShares + bobWalletShares + aliceWalletShares + charlieWalletShares + tonyWalletShares;
+        assertEq(walletTotalShares, totalSharesSum);
+    }
+
+    function _validateEpochShares() internal {
+        uint256 walletTotalShares = pushStaking.WALLET_TOTAL_SHARES();
+        for (uint256 i=genesisEpoch; i<=getCurrentEpoch(); ) {
+            assertLe(pushStaking.epochToTotalShares(i), walletTotalShares);
+            unchecked {
+                i++;
+            }
+        }
+    }
+
+    function _verifyTotalSharesConsistency(uint256 _walletSharesBeforeExecution) internal {
+        uint256 walletTotalSharesAfter = pushStaking.WALLET_TOTAL_SHARES();
+        assertEq(_walletSharesBeforeExecution, walletTotalSharesAfter);
+    }
 }
